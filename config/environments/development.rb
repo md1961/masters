96a1b30a89f487e5d847441552a958d0bebaf1cb
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-02-05 16:48:11 +0900

    Upgrade Rails from 4.0.3 to 4.2.6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Masters::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -19,7 +19,7 @@ Masters::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations
+  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
@@ -27,8 +27,15 @@ Masters::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  if ENV['TRUSTED_IP']
-    config.web_console.whitelisted_ips << ENV['TRUSTED_IP']
-    BetterErrors::Middleware.allow_ip!    ENV['TRUSTED_IP']
-  end
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
