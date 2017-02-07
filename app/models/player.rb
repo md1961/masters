4490864fb9e4bbcec047b630f14545de0014437b
Naoyuki Kumagai <kumagai@namaka>
2017-02-07 12:00:14 +0900

    Add Player#play() very preliminarily

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,6 +5,19 @@ class Player < ActiveRecord::Base
   has_one :ball
   has_one :shot, through: :ball
 
+  def play
+    create_ball!(shot: Shot.first_tee) unless ball
+    shot_judge = shot.judge(ball.result)
+    club = clubs.find_by(name: shot_judge.next_club.downcase)
+    result = club.swing
+    ball.result = result
+    self.shot = shot.next
+    shot_judge = shot.judge(result)
+    ball.shot_count += 1
+    save!
+    shot_judge
+  end
+
   def to_s
     "#{last_name}, #{first_name}"
   end
