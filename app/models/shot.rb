8c664a3577509530d1b48dfb7465dd9c6e738c4a
Naoyuki Kumagai <kumagai@namaka>
2017-02-07 11:58:33 +0900

    Add Shot.first_tee(), #judge() and #next()

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -3,4 +3,21 @@ class Shot < ActiveRecord::Base
   has_many :shot_judges
   has_many :balls
   has_many :players, through: :balls
+
+  def self.first_tee
+    find_by(hole: Hole.find_by(number: 1), number: 1)
+  end
+
+  def judge(result)
+    # TODO: Tons of more to do.
+    if result
+      result = result.sub(/\*\z/, '')
+    end
+
+    shot_judges.find_by(prev_result: result)
+  end
+
+  def next
+    Shot.find_by(hole: hole, number: number + 1, is_layup: false)
+  end
 end
