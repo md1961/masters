654efae36e2d97de475b4c5e83c8e074b9553e12
Naoyuki Kumagai <kumagai@namaka>
2017-02-07 14:47:02 +0900

    Modify Shot#judge() to handle result for 'All' or 'All other'

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -12,6 +12,13 @@ class Shot < ActiveRecord::Base
     # TODO: Tons of more to do.
     if result
       result = result.sub(/\*\z/, '')
+      prev_results = shot_judges.pluck(:prev_result)
+      if !prev_results.include?(result) && result =~ /\A[SML][LRC]-(Ch|P)\z/
+        suffix = Regexp.last_match(1)
+        re_result = /\AAll (:?other )?#{suffix}\z/
+        result = prev_results.find { |r| r =~ re_result }
+        raise StandardError, "Cannot find result matching #{re_result} in #{prev_results.inspect}" unless result
+      end
     end
 
     shot_judges.find_by(prev_result: result)
