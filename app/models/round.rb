87812d8188402f0555acbc3924183d52124498fd
Naoyuki Kumagai <kumagai@namaka>
2017-02-10 15:43:32 +0900

    Add after_create :setup_group_for_round_one to Round

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,19 @@ class Round < ActiveRecord::Base
   belongs_to :tournament
   has_many :areas
   has_many :groups
+
+  after_create :setup_group_for_round_one
+
+  private
+
+    def setup_group_for_round_one
+      players_shuffled = Player.all.map { |p| [p, rand] }.sort_by(&:last).map(&:first)
+      number = 1
+      players_shuffled.each_slice(2) do |players|
+        group = groups.create!(number: number, players: players)
+        group.groupings.find_by(player: players[0]).update!(play_order: 1)
+        group.groupings.find_by(player: players[1]).update!(play_order: 2)
+        number += 1
+      end
+    end
 end
