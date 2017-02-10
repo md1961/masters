5f5447a69fcc86b6105d06b1f67846bb9470a58b
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-02-11 08:28:52 +0900

    Correct Round to execute after_create only for 1st round

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,16 +3,22 @@ class Round < ActiveRecord::Base
   has_many :areas
   has_many :groups
 
-  after_create :setup_group_for_round_one, :create_areas_for_round_one
+  after_create :setup_group_for_round_one, :create_areas_for_round_one, if: :first_round?
 
-  NUM_PLAYERS_PER_GROUP = 2
+  def first_round?
+    number == 1
+  end
+
+  def num_players_per_group
+    2
+  end
 
   private
 
     def setup_group_for_round_one
       players_shuffled = Player.all.map { |p| [p, rand] }.sort_by(&:last).map(&:first)
       number = 1
-      players_shuffled.each_slice(NUM_PLAYERS_PER_GROUP) do |players|
+      players_shuffled.each_slice(num_players_per_group) do |players|
         group = groups.create!(number: number, players: players)
         group.groupings.find_by(player: players[0]).update!(play_order: 1)
         group.groupings.find_by(player: players[1]).update!(play_order: 2)
