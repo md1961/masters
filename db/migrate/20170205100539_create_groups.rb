8bfd64ce4305dc7a03707296756cc51d72e8a1d9
Naoyuki Kumagai <kumagai@namaka>
2017-02-10 15:34:35 +0900

    Fix Group by renaming attribute playing_at_id to area_id

--- db/migrate/20170205100539_create_groups.rb
+++ db/migrate/20170205100539_create_groups.rb
@@ -2,9 +2,9 @@ class CreateGroups < ActiveRecord::Migration
 
   def change
     create_table :groups do |t|
-      t.references :round     , index: true, foreign_key: true
-      t.integer    :number    , null: false
-      t.references :playing_at, index: true, foreign_key: true
+      t.references :round , index: true, foreign_key: true
+      t.integer    :number, null: false
+      t.references :area  , index: true, foreign_key: true
     end
   end
 end
