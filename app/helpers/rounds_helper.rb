6cf86a79e286a8b55ca8cd6aa45c39fd3d4f9c7a
Naoyuki Kumagai <kumagai@namaka>
2017-02-14 14:22:05 +0900

    Modify 'rounds#show' to display total score of all players

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -1,2 +1,7 @@
 module RoundsHelper
+
+  def total_score_display(player, round)
+    score = player.score_cards.find_by(round: round).total_score
+    score.zero? ? 'even' : format('%+d', score)
+  end
 end
--- app/models/score_card.rb
+++ app/models/score_card.rb
@@ -2,4 +2,18 @@ class ScoreCard < ActiveRecord::Base
   belongs_to :player
   belongs_to :round
   has_many :scores
+
+  def total_value
+    scores.sum(:value)
+  end
+
+  def total_score
+    total_value - total_par
+  end
+
+  private
+
+    def total_par
+      Hole.where(id: scores.pluck(:hole_id)).sum(:par)
+    end
 end
