6ce3fb6a5df7144eb33f2756591ff63b5f1f70de
Naoyuki Kumagai <kumagai@namaka>
2017-02-15 10:10:22 +0900

    Add total score display to each hole in score card

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -1,7 +1,7 @@
 module RoundsHelper
 
-  def total_score_display(player, round)
-    score = player.score_cards.find_by(round: round).total_score
+  def total_score_display(player = nil, round = nil, score: nil)
+    score = player.score_cards.find_by(round: round).total_score if player && round
     score.zero? ? 'even' : format('%+d', score)
   end
 
@@ -17,5 +17,15 @@ module RoundsHelper
     else
       ''
     end
+  end
+
+  def cum_score_display(index, scores)
+    score = scores[index]
+    return nil unless score
+    value = score.value
+    total_value = scores[0, index + 1].map { |s| s.try(:value) || 0 }.sum
+    par = score.hole.par
+    total_par = Hole.where('number <= ?', score.hole.number).sum(:par)
+    value && value == par ? nil : total_value - total_par
   end
 end
