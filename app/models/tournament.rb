22fb0bcc2cc4d69a18e4f42c7be2ff6717b01794
Naoyuki Kumagai <kumagai@namaka>
2017-02-16 08:56:57 +0900

    Add Tournament#current_round()

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,11 @@
 class Tournament < ActiveRecord::Base
   has_many :rounds
+
+  def current_round
+    rounds.find_by(is_current: true)
+  end
+
+  def finish
+    raise "End of Tournament (not implemented yet)"
+  end
 end
