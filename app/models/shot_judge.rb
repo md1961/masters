88d18d47648c16a0afdc739deb50e14d26084b26
Naoyuki Kumagai <kumagai@namaka>
2017-02-17 13:40:21 +0900

    Modify ShotJudge#to_s() not to include next_adjust of zero

--- app/models/shot_judge.rb
+++ app/models/shot_judge.rb
@@ -3,6 +3,12 @@ class ShotJudge < ActiveRecord::Base
 
   # FIXME: Handle 'or', hash
   def to_s
-    "#{next_use} (#{format('%+d', next_adjust).sub('+0', '0')})"
+    next_adjust = \
+      if next_adjust.nil? || next_adjust.zero?
+        ''
+      else
+        " (#{format('%+d', next_adjust)})"
+      end
+    "#{next_use}#{next_adjust}"
   end
 end
