b5410d1cf5a43b029bd2619f09d98889e2eec049
Naoyuki Kumagai <kumagai@namaka>
2017-02-17 14:03:12 +0900

    Add green to hole map

--- app/helpers/holes_helper.rb
+++ app/helpers/holes_helper.rb
@@ -1,10 +1,22 @@
 module HolesHelper
 
+  LANDING_GRIDS = [%w(SL ML LL), %w(SC MC LC), %w(SR MR LR)]
+
   def fairway_landings(hole)
     return [] if hole.par <= 3
-    [%w(SL ML LL), %w(SC MC LC), %w(SR MR LR)].map do |row|
-      row.map do |prev_result|
-        hole.shots.find_by(number: 2).shot_judges.find_by(prev_result: prev_result)
+    LANDING_GRIDS.map do |row|
+      row.map do |result|
+        hole.shots.find_by(number: 2).judge(result)
+      end
+    end
+  end
+
+  def green_landings(hole)
+    shot_number = hole.par - 1
+    LANDING_GRIDS.map do |row|
+      row.map do |result|
+        result += '-Ch'
+        result == 'MC-Ch' ? 'GREEN' : hole.shots.find_by(number: shot_number).judge(result)
       end
     end
   end
--- app/models/shot_judge.rb
+++ app/models/shot_judge.rb
@@ -3,12 +3,18 @@ class ShotJudge < ActiveRecord::Base
 
   # FIXME: Handle 'or', hash
   def to_s
-    next_adjust = \
+    next_adjust_display = \
       if next_adjust.nil? || next_adjust.zero?
         ''
       else
         " (#{format('%+d', next_adjust)})"
       end
-    "#{next_use}#{next_adjust}"
+    lands_display = case lands \
+      when 'Fairway', 'Near Green'
+        ''
+      else
+        "#{lands}, "
+      end
+    "#{lands_display}#{next_use}#{next_adjust_display}"
   end
 end
