ff525b059a1bd172456e8b5dc2698cd10e4c9d6d
Naoyuki Kumagai <kumagai@mimas>
2017-02-23 10:41:58 +0900

    Modify Ajax to proceed to use Ruby if-elsif-end for players_info scrolling

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -4,15 +4,19 @@ module RoundsHelper
     5
   end
 
+  def offset_from_bottom_of_to_display_in_players_info
+    2
+  end
+
   def num_players_in_leader_board
     10
   end
 
   def groups_for_players_info
-    offset_from_bottom_of_to_display = 2
     groups = @round.groups
     number_to_display = @round.group_to_display.number
-    n_scroll_out = number_to_display - (num_groups_in_players_info - offset_from_bottom_of_to_display)
+    n_scroll_out = number_to_display \
+                    - (num_groups_in_players_info - offset_from_bottom_of_to_display_in_players_info)
     if n_scroll_out <= 0
       groups
     else
