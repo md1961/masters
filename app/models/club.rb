290e041bdb87ed898cc03787a806f555429e2ec7
Naoyuki Kumagai <kumagai@mimas>
2017-02-23 11:58:49 +0900

    Alter Club to change direction of '-P' and '-Ch' result in certain probability

--- app/models/club.rb
+++ app/models/club.rb
@@ -8,6 +8,8 @@ class Club < ActiveRecord::Base
     name == 'putt'
   end
 
+  MAX_DICE_TO_RANDOMIZE_DIRECTION = 1
+
   # TODO: Alter direction of Ch and P randomly.
   # TODO: Interpolate distance randomly.
 
@@ -42,13 +44,21 @@ class Club < ActiveRecord::Base
     elsif raw_result == '(1)' && raw_dice != 66
       result = '1'
       info_add = ", converted to '1' for modified dice 66"
+    elsif raw_result =~ /\A([SML][LRC])-(P|Ch)/
+      dice = Dice.roll
+      if dice <= MAX_DICE_TO_RANDOMIZE_DIRECTION
+        direction     = Regexp.last_match(1)
+        result_suffix = Regexp.last_match(2)
+        result = add_random_direction(result_suffix, [direction])
+        info_add = ", direction converted on dice #{dice}"
+      end
     end
     @info = "'#{raw_result}' by #{self} on dice #{@info}" + (info_add ? info_add : '')
     result
   end
 
   def to_s
-    name =~ /[wi]\z/ ? name.upcase : name.capitalize
+    name =~ /(:?fw|[lms]i)\z/ ? name.upcase : name.capitalize
   end
 
   private
@@ -63,7 +73,9 @@ class Club < ActiveRecord::Base
       @@dices[index]
     end
 
-    def add_random_direction(raw_result)
-      "#{%w(SL SC SR ML MR LL LC LR).sample}-#{raw_result}"
+    def add_random_direction(raw_result, excludes = [])
+      excludes += %w(ML MR LL LC LR) if raw_result == 'P'
+      directions = %w(SL SC SR ML MR LL LC LR).reject { |dir| excludes.include?(dir) }
+      "#{directions.sample}-#{raw_result}"
     end
 end
