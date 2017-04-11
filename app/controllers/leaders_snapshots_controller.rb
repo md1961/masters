ea07b69ccbbcea97328110ebd60b13ef57c8eeb6
Naoyuki Kumagai <kumagai@mimas>
2017-04-11 15:26:05 +0900

    Add link to prev and next in 'leaders_snapshots#show' preliminarily

--- app/controllers/leaders_snapshots_controller.rb
+++ app/controllers/leaders_snapshots_controller.rb
@@ -2,5 +2,15 @@ class LeadersSnapshotsController < ApplicationController
 
   def show
     @leaders_snapshot = LeadersSnapshot.find(params[:id])
+    set_prev_and_next
   end
+
+  private
+
+    def set_prev_and_next
+      @prev = LeadersSnapshot.where(round: @leaders_snapshot.round).
+        where('seq_num < ?', @leaders_snapshot.seq_num).order(seq_num: :desc).limit(1).first
+      @next = LeadersSnapshot.where(round: @leaders_snapshot.round).
+        where('seq_num > ?', @leaders_snapshot.seq_num).order(:seq_num).limit(1).first
+    end
 end
