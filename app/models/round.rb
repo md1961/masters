9cc13a1915d9a015c2b28e91d9c79fdb5c973944
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-05-16 21:08:53 +0900

    Add Round#current_point_identifier() (T01R2-G05H13g)

--- app/models/round.rb
+++ app/models/round.rb
@@ -124,6 +124,12 @@ class Round < ActiveRecord::Base
     }.save!
   end
 
+  def current_point_identifier
+    player = current_group.players.first
+    format('T%02dR%d-G%02dH%02d%s', tournament.id, id, current_group.number,
+           player.ball.shot.hole.number, player.ball.shot.area.name.first.downcase)
+  end
+
   def <=>(other)
     return nil if other.nil? || !other.is_a?(Round)
     if tournament != other.tournament
