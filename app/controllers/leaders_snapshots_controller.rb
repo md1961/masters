9adb950396193cd30bbaa2b63b8a9a0e3086bf81
Naoyuki Kumagai <kumagai@mimas>
2017-05-18 11:50:58 +0900

    Modify 'leaders_snapshots#show' to browse in multiple rounds (T01R2-G09H13t)

--- app/controllers/leaders_snapshots_controller.rb
+++ app/controllers/leaders_snapshots_controller.rb
@@ -13,9 +13,12 @@ class LeadersSnapshotsController < ApplicationController
   private
 
     def set_prev_and_next
-      @prev = LeadersSnapshot.where(round: @leaders_snapshot.round).
-        where('seq_num < ?', @leaders_snapshot.seq_num).order(seq_num: :desc).limit(1).first
-      @next = LeadersSnapshot.where(round: @leaders_snapshot.round).
-        where('seq_num > ?', @leaders_snapshot.seq_num).order(:seq_num).limit(1).first
+      round = @leaders_snapshot.round
+      @prev = LeadersSnapshot.where(round: round).
+        where('seq_num < ?', @leaders_snapshot.seq_num).order(seq_num: :desc).limit(1).first \
+        || LeadersSnapshot.where(round: round.prev).order(seq_num: :desc).limit(1).first
+      @next = LeadersSnapshot.where(round: round).
+        where('seq_num > ?', @leaders_snapshot.seq_num).order(:seq_num).limit(1).first \
+        || LeadersSnapshot.where(round: round.next).order(:seq_num).limit(1).first
     end
 end
