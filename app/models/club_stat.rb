7913b75750b73a8b2419af1d8b5c12356122e01b
Naoyuki Kumagai <kumagai@mimas>
2017-06-30 10:05:40 +0900

    Create ClubStat for clubs other than Putt for now (T01R4-G08H16t)

---
+++ app/models/club_stat.rb
@@ -0,0 +1,83 @@
+class ClubStat
+
+  def initialize(club)
+    raise "Illegal argument '#{club.class}' (Club expected)" unless club.is_a?(Club)
+    @club = club
+  end
+
+  def drive_distance
+    return nil unless @club.drive?
+    calculate_average { |club_result|
+      case club_result.result
+      when /\AL/
+        10
+      when /\AM/
+        8
+      else
+        6
+      end
+    }
+  end
+
+  def fairway_keeping
+    return nil unless @club.drive?
+    calculate_average { |club_result|
+      club_result.result =~ /\A.C/ ? 1 : 0
+    }
+  end
+
+  def drive_pulling
+    return nil unless @club.drive?
+    calculate_average { |club_result|
+      club_result.result =~ /\A.L/ ? 1 : 0
+    }
+  end
+
+  def drive_pushing
+    return nil unless @club.drive?
+    calculate_average { |club_result|
+      club_result.result =~ /\A.R/ ? 1 : 0
+    }
+  end
+
+  def super_driving
+    return nil unless @club.drive?
+    calculate_average { |club_result|
+      club_result.result =~ /\*\z/ ? 1 : 0
+    }
+  end
+
+  def green_hitting
+    return nil if @club.drive? || @club.putt?
+    calculate_average { |club_result|
+      club_result.result =~ /\A(?:\(?\d+\)?|IN)\z/ ? 1 : 0
+    }
+  end
+
+  def green_hitting_distance
+    return nil if @club.drive? || @club.putt?
+    calculate_average { |club_result|
+      result = club_result.result
+      case result
+      when 'IN'
+        0
+      when '(1)'
+        0.5
+      when /\A\d+\z/
+        result.to_i
+      else
+        0
+      end
+    } / green_hitting
+  end
+
+  private
+
+    def calculate_average
+      @club.club_results.inject(0.0) { |sum, club_result|
+        weight = club_result.repdigit? ? 1 : 2
+        sum += yield(club_result) * weight
+        sum
+      } / 36
+    end
+end
