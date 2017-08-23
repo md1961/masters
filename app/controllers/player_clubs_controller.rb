4c713dd55ad71a386c796a10ec9c8d9fff29da23
Naoyuki Kumagai <kumagai@mimas>
2017-08-23 10:22:27 +0900

    Modify 'player_club_stat_suites#show' not to copy/swap same club(s)

--- app/controllers/player_clubs_controller.rb
+++ app/controllers/player_clubs_controller.rb
@@ -1,27 +1,31 @@
 class PlayerClubsController < ApplicationController
 
-  # "command"=>"copy_left_to_right", "club_name"=>"FW", "player_left_id"=>"15", "player_right_id"=>"1"
   def alter
     club_name = params[:club_name]
     player_left  = Player.find(params[:player_left_id])
     player_right = Player.find(params[:player_right_id])
-    begin
-      ActiveRecord::Base.transaction do
-        case params[:command]
-        when 'copy_left_to_right'
-          player_right.receive_club(club_name, player_left)
-        when 'copy_right_to_left'
-          player_left.receive_club(club_name, player_right)
-        when 'swap'
-          player_left.swap_clubs(club_name, player_right)
-        else
-          raise "ILLEGAL COMMAND '#{params[:command]}'"
+    alert = nil
+    if player_left.has_same_club?(club_name, player_right)
+      alert = 'Clubs are identical'
+    else
+      begin
+        ActiveRecord::Base.transaction do
+          case params[:command]
+          when 'copy_left_to_right'
+            player_right.receive_club(club_name, player_left )
+          when 'copy_right_to_left'
+            player_left .receive_club(club_name, player_right)
+          when 'swap'
+            player_left.swap_clubs(club_name, player_right)
+          else
+            raise "ILLEGAL COMMAND '#{params[:command]}'"
+          end
         end
+      rescue => e
+        raise e
       end
-    rescue => e
-      raise e
     end
-    redirect_to player_club_stat_suites_path
+    redirect_to player_club_stat_suites_path, alert: alert
   end
 
   def restore
